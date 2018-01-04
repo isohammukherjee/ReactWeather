b2b9377ff6b6ed1ef826c20d5994582907afb529
Soham Mukherjee <soham.mukherjee@teks.co.in>
2018-01-04 12:13:50 +0530

    Initial Commit

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+// var WeatherMessage = React.createClass({
+//   render: function () {
+//     var {temp, location} = this.props;
+//
+//     return(
+//       <div>
+//         <h3>It is {temp}&#8451; in {location}...</h3>
+//       </div>
+//     );
+//   }
+// });
+
+var WeatherMessage = ({temp, location}) => {
+  // var {temp, location} = props;
+  return(
+    <div>
+      <h3>It is {temp}&#8451; in {location}...</h3>
+    </div>
+  );
+};
+
+module.exports = WeatherMessage;
