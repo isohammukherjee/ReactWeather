1aa31a0c6840a96a5b9f42913e1301778c17c45b
Soham Mukherjee <soham.mukherjee@teks.co.in>
2018-01-08 16:07:59 +0530

    added scss

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ $(document).foundation();
 
 //App class
 
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Router history={hashHistory}>
